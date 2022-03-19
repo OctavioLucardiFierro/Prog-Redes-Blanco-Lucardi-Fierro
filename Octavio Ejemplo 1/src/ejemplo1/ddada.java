fefa43e89f1e8b6294e03d12c3596203dbbf9479
Nicol4sEsquivel <nikoezequiel57@gmail.com>
2022-03-18 21:42:53 -0300

    Se sube el ejemplo 1

---
+++ Octavio Ejemplo 1/src/ejemplo1/ddada.java
@@ -0,0 +1,16 @@
+package ejemplo1;
+
+public class ddada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("holamundo.");	
+		System.out.println("me quiero vivir");
+		celular objcelular = new celular();
+		objcelular.llamar("Octavio");
+		objcelular.cortarLlamada();
+		//objcelular.
+		
+	}
+
+}
