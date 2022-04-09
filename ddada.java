06f94b88f1e1fbff1583b7ecbffb890291a3fc98
octaviolucardifierro <83732890+OctavioLucardiFierro@users.noreply.github.com>
2022-04-08 21:08:30 -0300

    lo mismo de antes

---
+++ ddada.java
@@ -0,0 +1,19 @@
+package ejemplo1;
+
+public class ddada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("holamundo.");	
+		System.out.println("me quiero vivir");
+		celular objcelular = new celular();
+		objcelular.llamar("Octavio");
+		objcelular.cortarLlamada();
+		objcelular.setColor("aZUL");
+		objcelular.setMarca("Samsung");
+		objcelular.setModelo("J2 Prime");
+		objcelular.informarCaracteristicas();
+		
+	}
+
+}
