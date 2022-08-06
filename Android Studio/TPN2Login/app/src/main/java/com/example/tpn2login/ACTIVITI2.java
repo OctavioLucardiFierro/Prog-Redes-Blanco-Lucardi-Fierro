81d451cf54249f9c03d116d16ae0228b3829271d
OctavioLucardiFierro <octaviolf2016@gmail.com>
2022-08-05 21:44:03 -0300

    LOGIN LISTO

--- Android Studio/TPN2Login/app/src/main/java/com/example/tpn2login/ACTIVITI2.java
+++ Android Studio/TPN2Login/app/src/main/java/com/example/tpn2login/ACTIVITI2.java
@@ -2,7 +2,9 @@ package com.example.tpn2login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,11 +15,18 @@ public class ACTIVITI2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
 
+
         TextView Bienvenido = (TextView) findViewById(R.id.textView);
         Button Volver = (Button) findViewById(R.id.button2);
 
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activiti2);
+    }
+    public void holasoygerman(View v)
+    {
+        Intent atras = new Intent(this, MainActivity.class);
+        startActivity(atras);
     }
 }
