66d10e31b500392b58e5f56b24621a1221e613ba
OctavioLucardiFierro <octaviolf2016@gmail.com>
2022-09-01 11:32:50 -0300

    Se crea el Insert y un formulario de prueba

---
+++ Android_User/Registro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel = "stylesheet" href = "reister.css">
+    <title>Formulario INSERT</title>
+</head>
+<body>
+
+                <form method="post">
+                    <main class="contenido">
+                        <section>
+                                <form method="post" action="" name="signup-form">
+
+                                    <div class="form-element">
+                                        <label>Usuario</label>
+                                        <input type="text" name="Usuario"  required />
+                                    </div>
+
+                                    <div class="form-element">
+                                        <label>Email</label>
+                                        <input type="email" name="Mail" required />
+                                    </div>
+
+                                    <div class="form-element">
+                                        <label>Contraseña</label>
+                                        <input type="password" name="Contraseña" required />
+                                    </div>
+
+                                    <button type="submit" name="register" value="register">Registrar</button>
+                                </form>
+                                        <?php
+                                        include("insert.php");
+                                        ?>
+                        </section>
+                    </main>
+                </form>
+</body>
+</html>
